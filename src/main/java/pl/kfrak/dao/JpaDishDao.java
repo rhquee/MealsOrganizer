1c9ddc6590a7d504b40943ff2cd09c39bda02e18
rhquee <kfrak@proarray.com>
2020-04-24 20:50:35 +0200

    1st shot try connecting to db

---
+++ src/main/java/pl/kfrak/dao/JpaDishDao.java
@@ -0,0 +1,18 @@
+//package pl.kfrak.dao;
+//
+//import pl.kfrak.domain.Dish;
+//
+//import javax.persistence.EntityManager;
+//
+///**
+// * Created by kfrak on 23.04.2020.
+// */
+//public class JpaDishDao implements Dao<Dish> {
+//
+//    private EntityManager entityManager;
+//
+//    public JpaDishDao(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
+//
+//}
